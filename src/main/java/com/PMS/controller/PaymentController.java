e7359ac852e8cf7326f1d03886780e51db09194f
Chandan Kumar <chandankumar61006br@gmail.com>
2025-04-17 01:28:43 +0530

    updated api endpoint

--- src/main/java/com/PMS/controller/PaymentController.java
+++ src/main/java/com/PMS/controller/PaymentController.java
@@ -58,7 +58,7 @@ public class PaymentController {
         notify.put("email", true);
         paymentLinkRequest.put("notify", notify);
 
-        paymentLinkRequest.put("callback_url", "http://localhost:5173/upgrade_plan/success?planType=" + planType);
+        paymentLinkRequest.put("callback_url", "https://project-management-ivory.vercel.app/upgrade_plan/success?planType=" + planType);
 
         PaymentLink payment = razorpay.paymentLink.create(paymentLinkRequest);
 
